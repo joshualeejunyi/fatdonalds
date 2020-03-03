717ba6ee8f63d847444a12dee88adc2582345ab2
Joshua Lee <joshua.lee.99@hotmail.com>
2020-03-03 13:42:18 +0800

    initial push

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <?php
+        include "head.inc.php";
+    ?>
+    <body>
+        <?php
+            include "nav.inc.php";
+        ?>
+
+        
+    </body>
+</html>
---
+++ nav.inc.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-end">
+    <a class="navbar-brand" href="#">FatDonald's.</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse flex-grow-0 ml-auto mr-1" id="navbarSupportedContent">
+        <ul class="navbar-nav text-right">
+            <li class="nav-item active">
+                <a class="nav-link" href="about.php">About</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="menu.php">Our Menu</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="news.php">News</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+            </li>
+        </ul>
+        <button class="btn btn-primary">Account</button>
+    </div>
+</nav>
