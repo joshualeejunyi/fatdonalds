717c083e1a2afd38913f9b39092ef0dfd55f7a71
Joshua Lee <joshua.lee.99@hotmail.com>
2020-03-18 21:45:41 +0800

    push

--- about.php
+++ about.php
@@ -30,44 +30,54 @@
         </header>
         
         <main class="container">
-            <section class="card mb-3 h-100 about-card">
-                <div class="card-header text-white bg-dark text-center">
-                    <h4>
-                        Our History
-                    </h4>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">
-                        Fatdonald's was established in 2020 when a bunch of cyber security students gathered and thought: "What if our careers don't work out?"
+            <section class="row">
+                <div class="card-group">
+                    <div class="col-12 col-lg-6 col-xl-3">
+                        <div class="card mb-3 h-100 about-card">
+                            <div class="card-header text-white bg-dark text-center">
+                                <h4>
+                                    Our History
+                                </h4>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">
+                                    Fatdonald's was established in 2020 when a bunch of cyber security students gathered and thought: "What if our careers don't work out?"
 
-                        <br>
+                                    <br>
 
-                        As such, Fatdonald's was born: a satire about how we have been focused too much on our careers and not our health.
-                    </p>
-                </div>
-            </section>
-            <section class="card mb-3 h-100 about-card">
-                <div class="card-header text-white bg-dark text-center">
-                    <h4>
-                        Our Vision
-                    </h4>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">
-                        We envision a world with 100% healthy people, by giving them food so absurdly fat that they reflect on all their life choices.
-                    </p>
-                </div>
-            </section>
-            <section class="card mb-3 h-100 about-card">
-                <div class="card-header text-white bg-dark text-center">
-                    <h4>
-                        Our Mission
-                    </h4>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">
-                        To create really good but fattening food.
-                    </p>
+                                    As such, Fatdonald's was born: a satire about how we have been focused too much on our careers and not our health.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                        <div class="col-12 col-lg-6 col-xl-4">
+                        <div class="card mb-3 h-100 about-card">
+                            <div class="card-header text-white bg-dark text-center">
+                                <h4>
+                                    Our Vision
+                                </h4>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">
+                                    We envision a world with 100% healthy people, by giving them food so absurdly fat that they reflect on all their life choices.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 col-xl-4">
+                        <div class="card mb-3 h-100 about-card">
+                            <div class="card-header text-white bg-dark text-center">
+                                <h4>
+                                    Our Mission
+                                </h4>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">
+                                    To create really good but fattening food.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
