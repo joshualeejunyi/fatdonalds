4d572747a3efd1c932c1a39b0505816e7e2cda57
Niko <52725889+LilBitter@users.noreply.github.com>
2020-03-20 00:55:06 +0800

    cartmenu (wip)
    cartmenu (wip)

--- cart/cart.js
+++ cart/cart.js
@@ -1,38 +1,39 @@
-$(document).ready(function(){
-    $('$auto').load('ordermenu.php');
-    refresh();
-});
+/* global productID */
 
-function refresh()
-{
-    setTimeout(function() {
-        load('cart.php');
-        refresh();
-    }, 1000);
-}
-
-
-// Attach a submit handler to the form
-$( ".addCart" ).on("click", function() {
-    var id = $(this).prev();
-    var name = $(this).prev();
-//    var htmlToAppend = " <td>"+id+"</td> <td>"+ name +"</td>";
-//    console.log(htmlToAppend);
-    $.post("cart.php", {"id":id, "name":name});
-//    $("#cart").append(htmlToAppend);
-});
-
-
-//$(".addCart" ).on("click", function(){
-//    var form_data = $(this).serialize();
-//    $.ajax({
-//    url: "cart.php",
-//    type: "POST",
-//    dataType:"json",
-//    data: form_data
-//    }).done(function(data){
-//    $("#cart-container").html(data.products);
-//    button_content.html('Add to Cart');
+//$(document).ready(function(){
+////    $('$auto').load('ordermenu.php');
+////    refresh();
+////    
+////    
+//    $("#addCart").click(function(){
+//        $("#auto").load("cart.php");
 //    })
-//    e.preventDefault();
 //});
+
+//function refresh()
+//{
+//    setTimeout(function() {
+//        load('cart.php');
+//        refresh();
+//    }, 1000);
+//}
+
+
+
+
+
+$(".addCart").on("click", function ()
+{
+    $.ajax({
+        url: "ordermenu.php",
+        type: "POST",
+        data: {
+            hidden_ID: $(this).prev().val()
+        },
+        success: function (output_string)
+        {
+            console.log(output_string);
+            $('#auto').append(output_string);
+        }
+    });
+});
--- cart/ordermenu.php
+++ cart/ordermenu.php
@@ -1,20 +1,34 @@
-<?php   
-    include($_SERVER['DOCUMENT_ROOT'].'/auth/auth.php');
-?>
-
 <?php
-//    session_start();
-    
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-//        $_SESSION['hidden_ID'] = $_POST['hidden_ID'];
+    include($_SERVER['DOCUMENT_ROOT'] . '/auth/auth.php');
+    session_start();
+
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['hidden_ID'])) {
+        $_SESSION['id'] = $_POST['hidden_ID'];
         $id = $_POST['hidden_ID'];
-        $name = $_POST['hidden_Name'];
-        $_SESSION[$id]['name'] = $name;
+        $sql = "SELECT * FROM products WHERE productID = '". $id . "'";
+        $conn = dbconnect();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        echo '<table border="1"><tr>';
+        if ($stmt->rowCount() > 0) {
+            foreach ($stmt as $row) {
+                echo '<td>' . $row['productName'] . '</td>';
+                echo '<td>' . $row['productPrice'] . '</td>';
+            }
+        }
+
+        echo '</tr></table>';
+
+        $stmt = null;
+        $conn = null;
+        exit();
     }
-//    $_SESSION['products']= array();
-//    $itemInsert=array($_SESSION['hidden_ID'],$_SESSION['hidden_Name']);
-//    array_push($_SESSION['products'],$itemInsert);
-    
+    //    if($_SERVER["REQUEST_METHOD"] == "POST") {
+    ////        $_SESSION['hidden_ID'] = $_POST['hidden_ID'];
+    //        $id = $_POST['hidden_ID'];
+    //        $query = mysql_query('SELECT * FROM products WHERE productID ="'.$id);
+    //        $row = mysql_num_rows($query);
+    //    }
 ?>
 
 
@@ -22,154 +36,112 @@
 <html lang="en">
     <head>
         <?php
-            include($_SERVER['DOCUMENT_ROOT'].'/incl/head.inc.php');
-            include($_SERVER['DOCUMENT_ROOT'].'/incl/nav.inc.php');
-            include '/cart/cart.js' ;
+        include($_SERVER['DOCUMENT_ROOT'] . '/incl/head.inc.php');
+        include($_SERVER['DOCUMENT_ROOT'] . '/incl/nav.inc.php');
         ?>
- 
+        <script src="/cart/cart.js" defer></script>
     </head>
     <body>
         <main class="container">
-                                 <div id="auto"></div> 
-<!--                     //insert table here-->
-            <section id="MenuCards">
+            <div id="auto">
+                <!--                     //insert table here-->
+            </div> 
+            
+            <section id="content">
                 <div class="col-md-8 text-right header-box">
-		<a href="viewcart.php" class="cart-counter" id="cart-info" title="View Cart">            
-			<span class="cart-item" id="cart-container"><?php 
-			if(isset($_SESSION["products"])){
-				echo count($_SESSION["products"]); 
-			} else {
-				echo 0; 
-			}
-			?></span>
-		</a>
+
                 </div>
                 <section>
                     <h2>Products</h2>
                 </section>
-                 <div class="row datacards">
-                <?php
-                    try {
-                        $conn = dbconnect();
+                <div id="actions" class="collapsed" data-parent="#accordion">
+                    <div class="card-body">
+<?php
+try {
+    $conn = dbconnect();
 
-                        $queries = [];
-                        $parameters = [];
+    $queries = [];
+    $parameters = [];
 
-                        if ($keyword !== null) {
-                            $queries[] = 'productName LIKE ?';
-                            $parameters[] = '%' . $keyword . '%';
-                        }
+    if ($keyword !== null) {
+        $queries[] = 'productName LIKE ?';
+        $parameters[] = '%' . $keyword . '%';
+    }
 
-                        if ($catFilter !== null) {
-                            $queries[] = 'productCategory = ?';
-                            $parameters[] = $catFilter;
-                            $catstmt = $conn->prepare("SELECT DISTINCT productCategory from products WHERE productCategory = ? ORDER BY productCategory ASC");
-                            $catstmt->execute([$catFilter]);
-                        } else {
-                            $catstmt = $conn->prepare("SELECT DISTINCT productCategory from products ORDER BY productCategory ASC");
-                            $catstmt->execute();
-                        }
+    if ($catFilter !== null) {
+        $queries[] = 'productCategory = ?';
+        $parameters[] = $catFilter;
+        $catstmt = $conn->prepare("SELECT DISTINCT productCategory from products WHERE productCategory = ? ORDER BY productCategory ASC");
+        $catstmt->execute([$catFilter]);
+    } else {
+        $catstmt = $conn->prepare("SELECT DISTINCT productCategory from products ORDER BY productCategory ASC");
+        $catstmt->execute();
+    }
 
-                        $sql = "SELECT * from products";
+    $sql = "SELECT * from products";
 
-                        if ($queries) {
-                            $sql .= " WHERE ".implode(" AND ", $queries);
-                        }
+    if ($queries) {
+        $sql .= " WHERE " . implode(" AND ", $queries);
+    }
 
-                        $sql .= " ORDER BY productCategory ASC;";
+    $sql .= " ORDER BY productCategory ASC;";
 
-                        $stmt = $conn->prepare($sql);
-                        $stmt->execute($parameters);
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($parameters);
 
-                        if ($catstmt->rowCount() > 0) {
-                            if ($stmt->rowCount()>0) {
-                                foreach($catstmt as $catrow) {
-                                ?>
-                                    <div class="col-12 cardcol">
-                                        <div class="card mb-3 h-100">
-                                            <button class="collapsible">
-                                                 <?php echo $catrow["productCategory"];?>
-                                            </button>
-                                                <?php
-                                                    $stmt->execute($parameters);
-                                                    foreach($stmt as $row) {
-                                                        if ($row["productCategory"] === $catrow["productCategory"]) {
-                                                ?>         
-                                                    
-                                                            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                                                                <div class="card mb-3 h-100">
-                                                                    <?php
-                                                                        echo '<img class="card-img" src="data:image/jpeg;base64,'.base64_encode($row["productIMG"]).'"/>';
-                                                                    ?>
-                                                                    <div class="card-header">
-                                                                        <h4><?php echo $row["productName"];?></h4>
-                                                                    </div>
-                                                                    <div class="card-body">
-                                                                        <p class="card-text"><?php echo $row["productDesc"];?></p>
-                                                                        <h5 class="card-text">Price: $<?php echo $row["productPrice"];?></h5>
-                                                                    </div>
-                                                                    <div class="card-footer">
-                                                                        <input name="productID" type="hidden" value="<?php echo $row["productID"]; ?>" /> 
-                                                                        <input type="hidden" value="<?php echo $row["productName"]; ?>" /> 
-                                                                        <button class="btn btn-danger addCart">Add to Cart</button>
-                                                                    </div>
-                                                               
-                                                                </div>
-                                                            </div>
-                                                            
-                                                    
-                                                <?php
-                                                        }
-                                                    }
-                                                ?>
-                                        </div>        
-                                    </div>
-                <?php
-                                    }
-                                }
-                            }
-                    } catch (PDOException $e) {
-                        $errorMsg = "Connection failed: " . $e;
-                        $_SESSION['msg'] = $errorMsg;
-                    } finally {
-                        $conn = null;
-                        $stmt = null;
-                    }
+    if ($catstmt->rowCount() > 0) {
+        if ($stmt->rowCount() > 0) {
+            foreach ($catstmt as $catrow) {
                 ?>
-                    
-                </div>
-  
+                                        <div class="col-12 cardcol">
+                                            <div class="card mb-3 h-100">
+                                                <button class="collapsible">
+                                        <?php echo $catrow["productCategory"]; ?>
+                                                </button>
+                                        <?php
+                                        $stmt->execute($parameters);
+                                        foreach ($stmt as $row) {
+                                            if ($row["productCategory"] === $catrow["productCategory"]) {
+                                                ?>         
+
+                                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                                                            <div class="card mb-3 h-100">
+                                                            <?php
+                                                            echo '<img class="card-img" src="data:image/jpeg;base64,' . base64_encode($row["productIMG"]) . '"/>';
+                                                            ?>
+                                                                <div class="card-header">
+                                                                    <h4><?php echo $row["productName"]; ?></h4>
+                                                                </div>
+                                                                <div class="card-body">
+                                                                    <p class="card-text"><?php echo $row["productDesc"]; ?></p>
+                                                                    <h5 class="card-text">Price: $<?php echo $row["productPrice"]; ?></h5>
+                                                                </div>
+                                                                <div class="card-footer">
+                                                                    <input type="hidden" value="<?php echo $row["productID"] ?>"/> 
+                                                                    <button class="btn btn-danger addCart">Add to Cart</button>
+                                                                </div>                                                              
+                                                            </div>
+                                                        </div>
+                        <?php
+                    }
+                }
+                ?>
+                                            </div>        
+                                        </div>
+                <?php
+            }
+        }
+    }
+} catch (PDOException $e) {
+    $errorMsg = "Connection failed: " . $e;
+    $_SESSION['msg'] = $errorMsg;
+} finally {
+    $conn = null;
+    $stmt = null;
+}
+?>
+                    </div>
             </section>
-        </main>
-        
-        
+        </main>     
     </body>
-    
 </html>
-
-
-<style>
- .collapsible {
-   background-color: #777;
-   color: white;
-   cursor: pointer;
-   padding: 18px;
-   width: 100%;
-   border: none;
-   text-align: left;
-   outline: none;
-   font-size: 15px;
- }
-
- .active, .collapsible:hover {
-   background-color: #555;
- }
-
- .card-group {
-   padding: 0 18px;
-   max-height: 0;
-   overflow: hidden;
-   transition: max-height 0.2s ease-out;
-   background-color: #f1f1f1;
- }
- </style>
