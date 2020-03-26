f99f88c528f1b8dcedba34f8dc173f128e268d13
Joshua Lee <joshua.lee.99@hotmail.com>
2020-03-26 10:45:26 +0800

    fixed cards css

--- viewpost.php
+++ viewpost.php
@@ -7,6 +7,12 @@
         }
     }
 
+    if(isset($_GET['id'])) {
+        $newsID=$_GET['id'];
+    } else {
+        header('HTTP/1.0 404 not found'); 
+        include($_SERVER['DOCUMENT_ROOT'].'/auth/404.html');
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,26 +37,17 @@
                 <?php
                     try {
                         $conn = dbconnect();
-
-                        if ($keyword !== null) {
-                            $sql = "SELECT * from news WHERE title LIKE ? AND published = 1 ORDER BY created_timestamp DESC, edited_timestamp DESC";
-                            $stmt = $conn->prepare($sql);
-                            $stmt->execute([$keyword]);
-                        } else {
-                            $sql = "SELECT * from news WHERE published = 1 ORDER BY created_timestamp DESC, edited_timestamp DESC";
-                            $stmt = $conn->prepare($sql);
-                            $stmt->execute();
-                        }
+                        $sql = "SELECT * from news WHERE newsID = ?";
+                        $stmt = $conn->prepare($sql);
+                        $stmt->execute([$newsID]);
 
                         if ($stmt->rowCount()>0) {
                             foreach($stmt as $row) {
-                                ?>
+                        ?>
                                 <div class="col-12 ">
-                                    <a href="/viewpost.php?id=<?php echo $row["newsID"]; ?>">
                                     <div class="card mb-3 newscards">
                                         <div class="card-header prod-name">
                                             <h4><?php 
-                                                
                                                 echo $row["title"];
                                                 ?>
                                             </h4>
@@ -61,31 +58,17 @@
                                             </div>
                                         </div>
                                     </div>
-                                    </a>
                                 </div>
                     <?php
                             }
-                        } else {
-                    ?>
-                        <div class="col-12">
-                            <div class="card mb-3 newscards">
-                                <div class="card-body" style="color: black;">
-                                    <h4>
-                                        No Posts Found
-                                    </h4>
-                                </div>
-                            </div>
-                        </div>
-                    <?php
                         }
                     ?>
-                                        </div>
-                                    </div>        
-                                </div>
+                        </div>
+                    </div>        
+                </div>
                 <?php
                     } catch (PDOException $e) {
                         $errorMsg = "Connection failed: " . $e;
-                        $_SESSION['proderror'] = $errorMsg;
                     } finally {
                         $conn = null;
                         $stmt = null;
